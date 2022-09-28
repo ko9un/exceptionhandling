fb8df7537ee9022368c53e43c7e81dc83c9a07a2
ko9un <ktw1633@gmail.com>
2022-09-28 16:04:41 +0900

    예외처리 실행불가

---
+++ sec01_typeofexception/src/sec01_typeofexception/EX01_CheckedException/CheckedException.java
@@ -0,0 +1,40 @@
+package sec01_typeofexception.EX01_CheckedException;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+class A implements Cloneable{
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
+
+
+
+public class CheckedException {
+	
+	public static void main(String[] args) {
+		// 일반 예외 checked Exception 
+		
+		// 1. inerruptedException
+		Thread.sleep(1000);
+		
+		// 2. ClassNotFoundException
+		Class cls = Class.forName("java.lang.Object");
+		
+		// 3. IOException
+		InputStreamReader in = new InputStreamReader(System.in);
+		
+		//4. FileNotFoundException
+		FileInputStream fis = new FileInputStream("text.txt");
+		
+		//5. CloneNotSupppetedException
+		
+		A a1 = new A();
+		A a2 = (A)a1.clone();
+		
+		
+	}
+
+}
